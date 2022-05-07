b857abe814c3bacdeb41e2ec1a085d4889e4438b
Ivo Leist <ivo.leist@googlemail.com>
2022-05-07 20:08:59 +0200

    added custom login/auth to version controll

---
+++ client/src/components/cnag_auth.js
@@ -0,0 +1,30 @@
+import jwt_decode from 'jwt-decode';
+
+export  default  {
+
+  user: {
+    authenticated: false,
+    access_token: ''
+  },
+  getToken(){
+      if (this.user.keycloak!==undefined) return this.user.keycloak.token;
+      else return false
+  },
+  decoded(){
+      if (this.user.keycloak!==undefined) return jwt_decode(this.user.keycloak.token);
+      else return "empty";
+  },
+  setToken(user) {
+    console.log("set token entered")
+      this.user = user;
+      this.user.authenticated = true;
+  },
+  getUser(){
+      return this.user;
+  },
+  tokenExpired(){
+    const now = new Date()
+    const secondsSinceEpoch = Math.round(now.getTime() / 1000)
+    return this.decoded().exp > secondsSinceEpoch ? false : true;
+  }
+}
